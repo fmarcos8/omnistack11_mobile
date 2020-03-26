053178103ae259da66d62d30133eeca845ca18e0
Francisco Marcos <francisco-marcos@hotmail.com>
2020-03-26 19:40:28 -0300

    adicionando .env no projeto

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
+import { API_BASE_URL } from 'react-native-dotenv';
+
 const api = axios.create({
-  baseURL: 'http://192.168.78.56:3333/'
+  baseURL: API_BASE_URL
 });
 
 export default api;
